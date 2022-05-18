769df6263916b046c1ee7eee2bc4d1d5a3c3b8be
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-05-19 01:59:15 +0900

    add typealias

--- Sources/UIKitJapaneseTypealias/UIKitJapaneseTypealias.swift
+++ Sources/UIKitJapaneseTypealias/UIKitJapaneseTypealias.swift
@@ -1,6 +1,36 @@
-public struct UIKit_Japanese_Typealias {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
-    }
-}
+public typealias 端末 = UIDevice
+public typealias 窓 = UIWindow
+public typealias スクリーン = UIScreen
+public typealias シーン = UIScene
+
+public typealias 表示 = UIView
+public typealias 表示制御機 = UIViewController
+public typealias タブ表示制御機 = UITabBarController
+public typealias ナビゲーション表示制御機 = UINavigationController
+public typealias テーブル表示制御機 = UITableViewController
+public typealias コレクション表示制御機 = UICollectionViewController
+
+public typealias 大きさ = CGSize
+public typealias 位置 = CGPoint
+public typealias 長方形 = CGRect
+
+public typealias 反応機 = UIResponder
+
+public typealias 制御 = UIControl
+
+public typealias ラベル = UILabel
+public typealias テキスト表示 = UITextView
+public typealias テキストフィールド = UITextField
+public typealias スイッチ = UISwitch
+public typealias スライダー = UISlider
+public typealias スタック表示 = UIStackView
+public typealias ボタン = UIButton
+public typealias 画像表示 = UIImageView
+public typealias テーブル表示 = UITableView
+public typealias コレクション表示 = UICollectionView
+
+public typealias 色 = UIColor
+public typealias 画像 = UIImage
+public typealias フォント = UIFont
