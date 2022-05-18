d104c34481a0cf90d2551ddb1a77ce0fb74d6678
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-05-19 01:43:24 +0900

    rename

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "UIKit-Japanese-Typealias",
+    name: "UIKitJapaneseTypealias",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "UIKit-Japanese-Typealias",
-            targets: ["UIKit-Japanese-Typealias"]),
+            name: "UIKitJapaneseTypealias",
+            targets: ["UIKitJapaneseTypealias"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "UIKit-Japanese-Typealias",
+            name: "UIKitJapaneseTypealias",
             dependencies: []),
         .testTarget(
-            name: "UIKit-Japanese-TypealiasTests",
-            dependencies: ["UIKit-Japanese-Typealias"]),
+            name: "UIKitJapaneseTypealiasTests",
+            dependencies: ["UIKitJapaneseTypealias"]),
     ]
 )
