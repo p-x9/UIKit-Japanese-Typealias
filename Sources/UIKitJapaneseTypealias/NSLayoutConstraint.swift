c8e24bb66506359f4b9bf00f1d585020b99405d0
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-05-20 02:58:51 +0900

    add properties to NSLayoutConstraint

--- Sources/UIKitJapaneseTypealias/NSLayoutConstraint.swift
+++ Sources/UIKitJapaneseTypealias/NSLayoutConstraint.swift
@@ -12,6 +12,7 @@ import FoundationJapaneseTypealias
 public extension レイアウト制約 {
     
     typealias 関係 = レイアウト制約.Relation
+    typealias 属性 = レイアウト制約.Attribute
     
     var 優先度: UILayoutPriority {
         get {
@@ -44,6 +45,22 @@ public extension レイアウト制約 {
         }
     }
     
+    var 最初のアイテム: AnyObject? {
+        firstItem
+    }
+    
+    var 二番目のアイテム: AnyObject? {
+        secondItem
+    }
+    
+    var 最初の属性: 属性 {
+        firstAttribute
+    }
+    
+    var 二番目の属性: 属性 {
+        secondAttribute
+    }
+    
     var 最初の錨: レイアウト錨<AnyObject> {
         firstAnchor
     }
@@ -74,4 +91,36 @@ public extension レイアウト制約 {
     class func 無効化(_ 制約: [レイアウト制約]) {
         レイアウト制約.deactivate(制約)
     }
+}
+
+public extension レイアウト制約.関係 {
+    static let 以下 = Self.lessThanOrEqual
+    static let 等しい = Self.equal
+    static let 以上 = Self.greaterThanOrEqual
+}
+
+public extension レイアウト制約.属性 {
+    static let 左 = Self.left
+    static let 右 = Self.right
+    static let 上 = Self.top
+    static let 下 = Self.bottom
+    static let 前縁 = Self.leading
+    static let 後縁 = Self.trailing
+    static let 横幅 = Self.width
+    static let 高さ = Self.height
+    static let 最初の基準線 = Self.firstBaseline
+    static let 最後の基準線 = Self.lastBaseline
+    static let 左右中心 = Self.centerX
+    static let 上下中心 = Self.centerY
+    
+    static let 左余白 = Self.leftMargin
+    static let 右余白 = Self.rightMargin
+    static let 上余白 = Self.topMargin
+    static let 下余白 = Self.bottomMargin
+    static let 前縁余白 = Self.leadingMargin
+    static let 後縁余白 = Self.trailingMargin
+    static let 余白を含む左右中心 = Self.centerXWithinMargins
+    static let 余白を含む上下中心 = Self.centerYWithinMargins
+    
+    static let 属性でない = Self.notAnAttribute
 }
