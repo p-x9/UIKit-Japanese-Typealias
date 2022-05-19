325b44eabb653b3d5d9e326fabbdfa8f4b918e29
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-05-20 00:13:10 +0900

    add dependencies

--- Tests/UIKitJapaneseTypealiasTests/UIKitJapaneseTypealiasTests.swift
+++ Tests/UIKitJapaneseTypealiasTests/UIKitJapaneseTypealiasTests.swift
@@ -1,11 +1,6 @@
 import XCTest
-@testable import UIKit_Japanese_Typealias
+@testable import UIKitJapaneseTypealias
 
 final class UIKit_Japanese_TypealiasTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(UIKit_Japanese_Typealias().text, "Hello, World!")
-    }
+    
 }
