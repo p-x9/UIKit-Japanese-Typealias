a147114004e88144c0f9eef430f04e4870b21c80
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-06-15 03:48:48 +0900

    remove `@objc` attribute

--- Sources/UIKitJapaneseTypealias/UICollectionView/UICollectionViewDataSource.swift
+++ Sources/UIKitJapaneseTypealias/UICollectionView/UICollectionViewDataSource.swift
@@ -9,30 +9,55 @@
 import UIKit
 import FoundationJapaneseTypealias
 
-@objc
-public protocol コレクションビューデータソース: NSObjectProtocol {
-    @objc func コレクションビュー(_ コレクションビュー: コレクションビュー, セクション中のアイテム数 セクション: 整数) -> 整数
+public protocol コレクションビューデータソース: AnyObject {
+    func コレクションビュー(_ コレクションビュー: コレクションビュー, セクション中のアイテム数 セクション: 整数) -> 整数
     
-    @objc func コレクションビュー(_ コレクションビュー: コレクションビュー, セル インデックスパス: IndexPath) -> コレクションビューセル
+    func コレクションビュー(_ コレクションビュー: コレクションビュー, セル インデックスパス: IndexPath) -> コレクションビューセル
     
-    @objc optional func セクション数(コレクションビュー: コレクションビュー) -> 整数
+    func セクション数(コレクションビュー: コレクションビュー) -> 整数
     
-    @objc optional func コレクションビュー(_ コレクションビュー: コレクションビュー, 補足ビュー 種類: 文字列, インデックスパス: IndexPath) -> コレクション再利用可能ビュー
+    func コレクションビュー(_ コレクションビュー: コレクションビュー, 補足ビュー 種類: 文字列, インデックスパス: IndexPath) -> コレクション再利用可能ビュー
     
-    @objc optional func コレクションビュー(_ コレクションビュー: コレクションビュー, 移動可能か インデックスパス: IndexPath) -> 真理値
+    func コレクションビュー(_ コレクションビュー: コレクションビュー, 移動可能か インデックスパス: IndexPath) -> 真理値
     
-    @objc optional func コレクションビュー(_ コレクションビュー: コレクションビュー, 行を移動 移動元インデックスパス: IndexPath, 移動先インデックスパス: IndexPath)
-    
-    @available(iOS 14.0, *)
-    @objc optional func セクションインデックスのタイトル(コレクションビュー: コレクションビュー) -> [文字列]?
+    func コレクションビュー(_ コレクションビュー: コレクションビュー, 行を移動 移動元インデックスパス: IndexPath, 移動先インデックスパス: IndexPath)
     
     @available(iOS 14.0, *)
-    @objc optional func コレクションビュー(_ コレクションビュー: コレクションビュー, インデックスタイトルに対するインデックスパス タイトル: 文字列, インデックス: 整数) -> IndexPath
+    func セクションインデックスのタイトル(コレクションビュー: コレクションビュー) -> [文字列]?
+    
+    @available(iOS 14.0, *)
+    func コレクションビュー(_ コレクションビュー: コレクションビュー, インデックスタイトルに対するインデックスパス タイトル: 文字列, インデックス: 整数) -> IndexPath
+}
+
+extension コレクションビューデータソース {
+    func セクション数(コレクションビュー: コレクションビュー) -> 整数 {
+        1
+    }
+    
+    func コレクションビュー(_ コレクションビュー: コレクションビュー, 補足ビュー 種類: 文字列, インデックスパス: IndexPath) -> コレクション再利用可能ビュー {
+        fatalError(#function + "を実装してください")
+    }
+    
+    func コレクションビュー(_ コレクションビュー: コレクションビュー, 移動可能か インデックスパス: IndexPath) -> 真理値 {
+        false
+    }
+    
+    func コレクションビュー(_ コレクションビュー: コレクションビュー, 行を移動 移動元インデックスパス: IndexPath, 移動先インデックスパス: IndexPath) {}
+    
+    @available(iOS 14.0, *)
+    func セクションインデックスのタイトル(コレクションビュー: コレクションビュー) -> [文字列]? {
+        return nil
+    }
+    
+    @available(iOS 14.0, *)
+    func コレクションビュー(_ コレクションビュー: コレクションビュー, インデックスタイトルに対するインデックスパス タイトル: 文字列, インデックス: 整数) -> IndexPath {
+        fatalError(#function + "を実装してください")
+    }
 }
 
 extension UICollectionView: UICollectionViewDataSource {
     public func numberOfSections(in collectionView: UICollectionView) -> Int {
-        データソース?.セクション数?(コレクションビュー: collectionView) ?? 0
+        データソース?.セクション数(コレクションビュー: collectionView) ?? 0
     }
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -44,24 +69,24 @@ extension UICollectionView: UICollectionViewDataSource {
     }
     
     public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        データソース!.コレクションビュー!(collectionView, 補足ビュー: kind, インデックスパス: indexPath)
+        データソース!.コレクションビュー(collectionView, 補足ビュー: kind, インデックスパス: indexPath)
     }
     
     public func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
-        データソース?.コレクションビュー?(collectionView, 移動可能か: indexPath) ?? false
+        データソース?.コレクションビュー(collectionView, 移動可能か: indexPath) ?? false
     }
     
     public func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        データソース?.コレクションビュー?(collectionView, 行を移動: sourceIndexPath, 移動先インデックスパス: destinationIndexPath)
+        データソース?.コレクションビュー(collectionView, 行を移動: sourceIndexPath, 移動先インデックスパス: destinationIndexPath)
     }
     
     @available(iOS 14.0, *)
     public func indexTitles(for collectionView: UICollectionView) -> [String]? {
-        データソース?.セクションインデックスのタイトル?(コレクションビュー: collectionView)
+        データソース?.セクションインデックスのタイトル(コレクションビュー: collectionView)
     }
     
     @available(iOS 14.0, *)
     public func collectionView(_ collectionView: UICollectionView, indexPathForIndexTitle title: String, at index: Int) -> IndexPath {
-        データソース!.コレクションビュー!(collectionView, インデックスタイトルに対するインデックスパス: title, インデックス: index)
+        データソース!.コレクションビュー(collectionView, インデックスタイトルに対するインデックスパス: title, インデックス: index)
     }
 }
